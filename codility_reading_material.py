c9cf919faa64cbf28d431ba27df67afd6a91e335
Gbenga Ojo <gbenga@gbengaojo.com>
2015-11-26 15:08:50 -0600

    function counting defined

--- codility_reading_material.py
+++ codility_reading_material.py
@@ -1,9 +1,16 @@
-// time complexity
+# time complexity
 def dominant(n):
    result = 0
    for i in xrange(n):
       result += 1
    return result
 
-// counting elements
+# counting elements
+def counting(A, m):
+   n = len(A)
+   count = [0] * (m + 1)
+   for k in xrange(n):
+      count[A[k]] += 1
+   return count
 
+print counting([1,2,3,4], 4);
