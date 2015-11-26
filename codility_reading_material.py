3ce3383255b60d5430f5b27eef08a1c5045b798d
Gbenga Ojo <gbenga@gbengaojo.com>
2015-11-26 17:02:12 -0600

    prefix sum

--- codility_reading_material.py
+++ codility_reading_material.py
@@ -31,3 +31,29 @@ def slow_solution(A, B, m):
          sum_a -= change
          sum_b += change
    return False
+
+# Exercise 2.3
+def fast_solution(A, B, m):
+   n = len(A)
+   sum_a = sum(A)
+   sum_b = sum(B)
+   d = sum_b - sum_a
+   if d % 2 == 1:
+      return False
+   d //= 2
+   count = counting(A, m)
+   for i in xrange(n):
+      if 0 <= B[i] - d and B[i] - d <= m and count[B[i] - d] > 0:
+         return True
+   return False
+
+
+# Prefix sums
+# https://codility.com/media/train/3-PrefixSums.pdf
+
+def prefix_sums(A):
+   n = len(A)
+   P = [0] * (n + 1)
+   for k in xrange(1, n + 1):
+      P[k] = P[k - 1] + A[k - 1]
+   return P
