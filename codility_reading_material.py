014dc049418e713dea341d0a6e5f065428be1700
Gbenga Ojo <gbenga@gbengaojo.com>
2015-11-26 20:38:39 -0600

    switching gears temporarily

--- codility_reading_material.py
+++ codility_reading_material.py
@@ -58,3 +58,14 @@ def prefix_sums(A):
 
 def count_total(P, x, y):
    return P[y + 1] - P[x]
+
+# 3.3
+def mushrooms(A, k, m):
+   n = len(A)
+   result = 0
+   pref = prefix_sums(A)
+   for p in xrange(min(m, k) + 1:
+      left_pos = k - p
+      right_pos = min(n - 1, max(k, k + m - 2 * p))
+      result = max(result, count_total(pref, left_pos, right_pos))
+   for  p in xrange(min(m + 1, n - k
